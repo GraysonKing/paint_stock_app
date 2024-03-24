e3d98a1888e8c532803c74fcd5ed395f11e95a24
Grayson <grayson.king065@gmail.com>
2024-03-24 13:25:31 -0700

    Restrict stock input to only numbers

--- resources/js/Components/PaintCard.jsx
+++ resources/js/Components/PaintCard.jsx
@@ -4,7 +4,9 @@ const PaintCard = ({ paint, onUpdate }) => {
     return (
         <div className="bg-white shadow-sm rounded-lg sm:w-full md:w-1/2 lg:w-1/3 mb-4">
             <div className="p-4">
-                <h5 className="text-gray-900 font-bold text-lg mb-2">{paint.name}</h5>
+                <h5 className="text-gray-900 font-bold text-lg mb-2">
+                    {paint.name}
+                </h5>
                 <p className="text-gray-700 text-sm mb-2">
                     Colour Code: {paint.colour_code}
                 </p>
@@ -20,6 +22,11 @@ const PaintCard = ({ paint, onUpdate }) => {
                         type="number"
                         className="border px-2 py-1 rounded-md focus:ring-1 focus:ring-blue-500 w-full w-auto [appearance:textfield]"
                         defaultValue={paint.stock}
+                        onKeyDown={(event) => {
+                            if (!/[0-9]/.test(event.key)) {
+                                event.preventDefault();
+                            }
+                        }}
                         onBlur={(e) => onUpdate(paint, e.target.value)}
                     />
                 </div>
